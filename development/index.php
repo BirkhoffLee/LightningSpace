6bf78bad6891c2230016beb2a9a5fec003ec21e7
BirkhoffLee <b@irkhoff.com>
2014-07-13 18:45:02 +0800

    The index.

--- development/index.php
+++ development/index.php
@@ -1,4 +1,11 @@
 <?php
+/*
+  @package  index.php
+  @since    1.0       First version.
+  @author   Birkhoff  b@irkhoff.com
+  @see      http://b3h.xyz
+  @License  http://b3h.xyz/License/LS
+*/
 require 'LS-common.php';
 
 if(LS-UPLOADED) {
