8009a64f76fb50bbd8f7de4ead2dc04184a7202e
BirkhoffLee <b@irkhoff.com>
2014-07-13 19:13:30 +0800

    The index.

--- development/index.php
+++ development/index.php
@@ -8,12 +8,30 @@
 */
 require 'LS-common.php';
 
-if(LS-UPLOADED) {
 
+if(LS-UPLOADED) {
 	$FileName = $LS_UPLOADED_FILE['FileName'];
 	$FileType = $LS_UPLOADED_FILE['FileType'];
 	$FileSize = $LS_UPLOADED_FILE['FileSize'];
 	$FileKey = $LS_UPLOADED_FILE['FileKey'];
-
+	echo $LightningSpace->Templet(
+		'uploaded.html',
+		array(
+			'SiteName' => LS-SITE-NAME,
+			'Description' => LS-DESCRIPTION,
+			'FileName' => $FileName,
+			'FileType' => $FileType,
+			'FileSize' => $FileSize,
+			'FileKey' => $FileKey
+		)
+	);
 }
-?>
+
+
+echo $LightningSpace->Templet(
+	'index.html',
+	array(
+		'SiteName' => LS-SITE-NAME,
+		'Description' => LS-DESCRIPTION
+	)
+);
