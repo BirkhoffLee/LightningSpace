2b5cdbfe86aac4b12d5eb6fce1b872c08bdac2f0
BirkhoffLee <b@irkhoff.com>
2014-07-13 16:18:53 +0800

    The index.

---
+++ development/index.php
@@ -0,0 +1,12 @@
+<?php
+require 'LS-common.php';
+
+if(LS-UPLOADED) {
+
+	$FileName = $LS_UPLOADED_FILE['FileName'];
+	$FileType = $LS_UPLOADED_FILE['FileType'];
+	$FileSize = $LS_UPLOADED_FILE['FileSize'];
+	$FileKey = $LS_UPLOADED_FILE['FileKey'];
+
+}
+?>
