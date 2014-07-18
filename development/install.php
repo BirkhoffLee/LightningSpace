b835c35e504bfa2339e6fa7bd89ea75dcc2253ff
BirkhoffLee <b@irkhoff.com>
2014-07-18 14:42:56 +0800

    Provides friendly Installation

---
+++ development/install.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  @package  install.php
+  @since    1.0       First version.
+  @author   Birkhoff  b@irkhoff.com
+  @see      http://b3h.xyz
+  @License  http://b3h.xyz/License/LS
+*/
+require 'LS-common.php';
+System::Initalize();
+System::Install();
